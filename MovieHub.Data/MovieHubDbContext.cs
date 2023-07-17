7081fcf22e6e5c30719a7e49d43d1f4cfa200e72
Administrator <Administrator@SLAVI1PC>
2023-07-17 20:05:18 +0300

    Add project files.

---
+++ MovieHub.Data/MovieHubDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieHub.Data
+{
+    public class MovieHubDbContext : IdentityDbContext
+    {
+        public MovieHubDbContext(DbContextOptions<MovieHubDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
