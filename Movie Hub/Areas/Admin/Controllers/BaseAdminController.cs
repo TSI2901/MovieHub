f32fd8d9c673322e4fc23b22778c9645fb67cf68
TSI2901 <90517326+TSI2901@users.noreply.github.com>
2023-08-11 16:43:23 +0300

    Add admin area

---
+++ Movie Hub/Areas/Admin/Controllers/BaseAdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using static MovieHub.Common.GeneralApplicationConstants;
+namespace Movie_Hub.Areas.Admin.Controllers
+{
+    [Area(AdminArea)]
+    [Authorize(Roles = AdminRole)]
+    public class BaseAdminCotroller : Controller
+    {
+        public string GetUserID()
+        {
+            string id = string.Empty;
+            if (User != null)
+            {
+                id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+            return id;
+        }
+    }
+}
